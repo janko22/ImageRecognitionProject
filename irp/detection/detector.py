92a65794437752981c1b6349b22edbd9e6f080dd
Rafal Jankowski <jankowskirafal20@gmail.com>
2024-12-21 15:26:39 +0100

    created separate Detector class to separate detection logic from VideoPlayer class

---
+++ irp/detection/detector.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+from irp.detection.detection import Detection
+
+class Detector:
+    def __init__(self, weights, cfg):
+        self.net = cv2.dnn.readNet(weights, cfg)
+
+    def detect_humans(self, frame):
+        humans = []
+
+        (height, width) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
+        self.net.setInput(blob)
+
+        output_layer_name = self.net.getUnconnectedOutLayersNames()
+        output_layers = self.net.forward(output_layer_name)
+
+        for output in output_layers:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+
+                if class_id == 0 and confidence > 0.5:
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    x = int(center_x - w /2)
+                    y = int(center_y - h /2)
+
+                    humans.append(Detection([x, y, w, h], class_id, confidence))
+
+        return humans
--- irp/vcap/video_capture.py
+++ irp/vcap/video_capture.py
@@ -1,14 +1,13 @@
 import cv2
-import numpy as np
 
-from irp.detection.detection import Detection
+from irp.detection.detector import Detector
 
 
 # Class Responsible for displaying Video Player with keyboard controls
 class VideoManager:
     def __init__(self, path):
         self.path = path
-        self.net = cv2.dnn.readNet("./yolov3.weights", "./yolov3.cfg")
+        self.detector = Detector("./yolov3.weights", "./yolov3.cfg")
         self.vcap = cv2.VideoCapture(path+"%06d.jpg")
 
     # public function play video and handle the controls
@@ -25,7 +24,7 @@ class VideoManager:
                 if not ret:
                     is_paused = True
                     continue
-                detections = self.__detect(frame)
+                detections = self.detector.detect_humans(frame)
                 self.__open_video(frame, detections)
 
             # controls
@@ -39,14 +38,14 @@ class VideoManager:
                 if is_paused:
                     ret, frame = self.vcap.read()
                     if ret:
-                        detections = self.__detect(frame)
+                        detections = self.detector.detect_humans(frame)
                         self.__open_video(frame, detections)
             elif key in (83, ord('e')):
                 self.__rewind(60)
                 if is_paused:
                     ret, frame = self.vcap.read()
                     if ret:
-                        detections = self.__detect(frame)
+                        detections = self.detector.detect_humans(frame)
                         self.__open_video(frame, detections)
 
         self.vcap.release()
@@ -71,34 +70,3 @@ class VideoManager:
         cv2.imshow('Video ' + self.path, frame)
         cv2.namedWindow('Video ' + self.path, cv2.WINDOW_NORMAL)
         cv2.setWindowProperty('Video ' + self.path, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    def __detect(self, frame):
-        humans = []
-
-        (height, width) = frame.shape[:2]
-
-        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
-        self.net.setInput(blob)
-
-        output_layer_name = self.net.getUnconnectedOutLayersNames()
-        output_layers = self.net.forward(output_layer_name)
-
-        for output in output_layers:
-            for detection in output:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
-
-                if class_id == 0 and confidence > 0.5:
-                    center_x = int(detection[0] * width)
-                    center_y = int(detection[1] * height)
-
-                    w = int(detection[2] * width)
-                    h = int(detection[3] * height)
-
-                    x = int(center_x - w /2)
-                    y = int(center_y - h /2)
-
-                    humans.append(Detection([x, y, w, h], class_id, confidence))
-
-        return humans
